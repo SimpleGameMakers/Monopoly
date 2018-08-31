157fd719d5b0110022ddf306d1549eb1e21d1b55
SimpleGameMakers <SimpleGameMakers@gmail.com>
2018-08-31 20:52:47 +0300

    First code

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Controller : MonoBehaviour
+{
+    public static T Create<T>(string gameObjectName) where T:Controller
+    {
+        return new GameObject(gameObjectName).AddComponent<T>();
+    }
+
+
+    #region MonoBehaviour
+    protected virtual void Awake()
+    {
+        /* empty */
+    }
+
+
+    protected virtual void Update()
+    {
+        /* empty */
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ServiceManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ServiceManager
+{
+    public readonly static EventService EventService;
+    public readonly static ControlService ControlService;
+
+
+    static ServiceManager()
+    {
+        EventService = Service.Create<EventService>();
+        ControlService = Service.Create<ControlService>();
+    }
+}
